de5f5c2ee39fef8d6faf6476bbcba6eba6bc3b19
Sandalu Umayanga <147733995+sandalu-umayanga@users.noreply.github.com>
2025-07-06 11:54:25 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://192.168.43.192:8000/api/", // Adjust to your API's base URL
+  baseURL: "http://54.83.76.56/api/", // Adjust to your API's base URL
 });
 
 // Add interceptor for JWT
